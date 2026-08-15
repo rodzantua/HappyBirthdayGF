4e03719469472ac8b63dce5f329602277c0fc9ce
rodzantua <rzantua@addu.edu.ph>
2026-08-15 19:23:06 +0800

    greeting update

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('mousemove', (e) => {
 });
 
 // Typing effect for greeting
-const greetingText = "Hey You Know What! You're the most adorable human i ever met! 💖";
+const greetingText = "To the strongest, most beautiful, and amazing person ever!💖";
 const greetingElement = document.querySelector('.greeting');
 let charIndex = 0;
 
